4fbcc10c8cd3cab4779a9bf0e178a2c0524817ae
lorenzo.lepore <91348041+lorenzo-lepore@users.noreply.github.com>
2024-07-16 18:02:53 +0200

    Nearby supermarkets page, new navigation system, Kotlin update

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,10 @@ import 'package:flutter/material.dart';
 import './screens/home_screen.dart';
 
 import 'package:openfoodfacts/openfoodfacts.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: '.env');
   runApp(const MyApp());
 }
 
